979cc1ba327627927d15843e9162e5babf8c9458
Andrew Hulme <andrewhulme1993@gmail.com>
2020-04-16 17:31:17 +0100

    Release working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,13 @@ describe DockingStation do
 
   it { expect(DockingStation.new).to respond_to(:release_bike)}
 
+  it "docking station releases working bike" do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike.working?).to be true
+  end
+
 end
+
+#Add a test to your DockingStation specification that
+#a) gets a bike, and then b) expects the bike to be working
